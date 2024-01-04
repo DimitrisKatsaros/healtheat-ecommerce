1e71531772c9c36e4279b9621efc5e941ab7cf9e
DimitrisKatsaros <40338023+DimitrisKatsaros@users.noreply.github.com>
2024-01-04 15:30:09 +0200

    Delete 01-back-end directory

--- 01-back-end/src/main/java/com/gmail/katsaros/s/dimitris/ecommerce/dao/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.katsaros.s.dimitris.ecommerce.dao;
-
-import com.gmail.katsaros.s.dimitris.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- 01-back-end/src/main/java/com/gmail/katsaros/s/dimitris/ecommerce/service/CheckOutService.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.katsaros.s.dimitris.ecommerce.service;
-
-import com.gmail.katsaros.s.dimitris.ecommerce.dto.Purchase;
-import com.gmail.katsaros.s.dimitris.ecommerce.dto.PurchaseResponse;
-
-public interface CheckOutService {
-
-    PurchaseResponse placeOrder(Purchase purchase);
-}
